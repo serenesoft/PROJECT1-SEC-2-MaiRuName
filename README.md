7acd98717302e4b112f75428d9e387c50ec8220c
NatchanonBoonchu <88813990+NatchanonBoonchu@users.noreply.github.com>
2023-02-12 16:09:17 +0700

    Update Boonchu's code
    ทำเครื่องคิดเลขกับแสดงสมการแล้ว เหลือแต่ทำ History

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-Member
+# calculator-project
 
-64130500081 นาย เศรษฐบฎี บุญพิทักษ์
+This template should help get you started developing with Vue 3 in Vite.
 
-64130500083 นายสหรัฐ คนึงเหตุ
+## Recommended IDE Setup
 
-64130500098 นายณัฐชนน บุญชู
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
 
-64130500118 นางสาวชนากานต์ นิลวัฒน์
+## Customize configuration
 
-64130500121 นายรามิล ปิยานุลักษณ์
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
--- src/App.vue
+++ src/App.vue
@@ -2,130 +2,146 @@
 export default {
   data() {
     return {
-      previous: null,
       current: '',
-      operator:null,
+      previousNumber: '',
+      operator: null,
+      currentOperator: null,
       operatorClicked: false,
+      equation: ''
     }
   },
-  methods:{
-    clear(){
+  methods: {
+    clear() {
       this.current = ''
+      this.equation = ''
     },
-    sign(){
-      this.current = this.current.charAt(0) === '-' ?  this.current.slice(1): `-${this.current}`
+    sign() {
+      this.current = this.current.charAt(0) === "-" ?
+      this.current.slice(1) : `-${this.current}`
     },
-    setPrevious(){
-      this.previous=this.current 
-      this.operatorClicked = true
+    del() {
+      this.current = `${this.current.slice(0, -1)}`
     },
-    percent(){
-      this.current = `${parseFloat(this.current) / 100}`
-    },
-    divide(){
-      this.operator = (a,b) => a/b
-      this.setPrevious()
-    }
-    ,
-    multiply(){
-      this.operator = (a,b) => a*b
-      this.setPrevious()
-    },
-    minus(){
-      this.operator = (a,b) => a-b
-      this.setPrevious()
-    },
-    plus(){
-      this.operator = (a,b) => a+b
-      this.setPrevious()
-    },
-    equal(){
-      this.current = `${this.operator(parseFloat(this.previous),parseFloat(this.current))}`
-      this.previous = null
-    },
-    append(number){
-      if(this.operatorClicked){
-        this.current = '';
-        this.operatorClicked = false;
+    append(number) {
+      if(this.operatorClicked) {
+        this.current = ''
+        this.operatorClicked = false
       }
       this.current = `${this.current}${number}`
     },
-    dot(){
-      if(this.current.indexOf('.') === -1)
-      this.append('.')
+    setPreviousNumber() {
+      this.previousNumber = this.current
+      this.operatorClicked = true
+    },
+    dot() {
+      if(this.current.includes('.') || this.current === ''){ // ดักไม่ให้กด'.'ซ้ำและกดตัวแรก
+        return ;
+      }else {
+        this.append('.')
+      }
+    },
+    divide(){
+      this.operator = (a, b) => a / b
+      this.currentOperator = '÷'
+      this.setEquation()
+      this.setPreviousNumber()
+    },
+    multiply() {
+      this.operator = (a, b) => a * b
+      this.currentOperator = 'x'
+      this.setEquation()
+      this.setPreviousNumber()
+    },
+    minus() {
+      this.operator = (a, b) => a - b
+      this.currentOperator = '-'
+      this.setEquation()
+      this.setPreviousNumber()
+    },
+    plus() {
+      this.operator = (a, b) => a + b
+      this.currentOperator = '+'
+      this.setEquation()
+      this.setPreviousNumber()
+    },
+    setEquation() {
+      this.equation = `${this.current} ${this.currentOperator}`
+    },
+    equal() {
+      this.equation = `${this.previousNumber} ${this.currentOperator} ${this.current} = `
+      this.current = this.operator(parseFloat(this.previousNumber), parseFloat(this.current))
+      this.previousNumber = null
     }
   }
 }
 </script>
-
+ 
 <template>
-  <div class="calculator">
-    <div class="display">{{current || '0'}}</div>
-    <div @click="clear" class="btn">C</div>
-    <div @click="sign" class="btn">+/-</div>
-    <div @click="percent" class="btn">%</div>
-    <div @click="divide" class="btn , operator">÷</div>
-    <div @click="append('7')" class="btn">7</div>
-    <div @click="append('8')" class="btn">8</div>
-    <div @click="append('9')" class="btn">9</div>
-    <div @click="multiply" class="btn , operator">x</div>
-    <div @click="append('4')" class="btn">4</div>
-    <div @click="append('5')" class="btn">5</div>
-    <div @click="append('6')" class="btn">6</div>
-    <div @click="minus" class="btn , operator">-</div>
-    <div @click="append('1')" class="btn">1</div>
-    <div @click="append('2')" class="btn">2</div>
-    <div @click="append('3')" class="btn">3</div>
-    <div @click="plus" class="btn , operator">+</div>
-    <div @click="append('0')" class="btn , zero">0</div>
-    <div @click="dot" class="btn">.</div>
-    <div @click="equal" class="btn , operator">=</div>
+  
+<div class="bg-gray-400 flex items-center justify-center h-screen">
+  <div class="bg-slate-50 p-10 rounded-lg ">
+    <h1 class="text-5xl text-center pb-7 font-semibold">Calculator</h1>
+    <div class="calculator text-3xl w-96 p-8">
+      <div class="display bg-slate-800  col-span-4 pl-2 rounded-lg mb-1 text-right pr-2 relative">
+        <div class="text-gray-200 text-2xl ">
+          {{ equation }}
+        </div>
+        <div class="text-white text-5xl font-bold absolute bottom-1 right-2">
+          {{ current || 0 }}
+        </div>
+      </div>
+      <div @click="clear()" class="btn">C</div>
+      <div @click="sign()" class="btn">+/-</div>
+      <div @click="del()" class="btn">←</div>
+      <div @click="divide()" class="btn operator">÷</div>
+      <div @click="append('7')" class="btn">7</div>
+      <div @click="append('8')" class="btn">8</div>
+      <div @click="append('9')" class="btn">9</div>
+      <div @click="multiply()" class="btn operator">x</div>
+      <div @click="append('4')" class="btn">4</div>
+      <div @click="append('5')" class="btn">5</div>
+      <div @click="append('6')" class="btn">6</div>
+      <div @click="minus()" class="btn operator">-</div>
+      <div @click="append('1')" class="btn">1</div>
+      <div @click="append('2')" class="btn">2</div>
+      <div @click="append('3')" class="btn">3</div>
+      <div @click="plus()" class="btn operator">+</div>
+      <div @click="append('0')" class="btn col-span-2">0</div>
+      <div @click="dot()" class="btn">.</div>
+      <div @click="equal()" class="btn operator">=</div>
+    </div>
   </div>
+</div>
 </template>
-
+ 
 <style scoped>
 .calculator {
-  margin: auto;
-  width: 500px;
-  padding: 50px;
-  border-radius: 25px;
-  background-color: #3D5875;
-  font-size: 40px;
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: minmax(50px, auto);
-
+  grid-template-columns: repeat(4, 80px);
+  grid-template-rows: repeat(6, 80px);
+  background-color: #e5e7eb;
+  border-radius: 5%;
+  user-select: none;
 }
-
-.display {
-  grid-column: 1/5;
-  background-color: #333;
-  border-radius: 10px;
-  color: white;
-  text-align: center;
-  padding: 10px;
-  margin-bottom: 10px;
-}
-
-.zero {
-  grid-column: 1/3;
-}
-
 .btn {
-  background-color: #f2f2f2;
-  border: 1px solid #999;
-  padding: 20px;
-  margin: 5px;
+  background-color: #cbd5e1;
+  border: 1px solid black;
   border-radius: 10px;
   text-align: center;
+  margin: 3px;
+  padding: 14px;
 }
-.btn:hover{
-  cursor: pointer;
-  background: #3D5875;
+.btn:hover {
+  transition-duration: 0.4s;
+  background-color:  #94a3b8;
+  color: white;
 }
-
 .operator {
   background-color: orange;
   color: white;
+}
+.operator:hover {
+  background-color: rgb(250, 200, 109);
+  color: black;
 }
 </style>
