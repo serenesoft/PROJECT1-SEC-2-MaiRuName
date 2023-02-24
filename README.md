b7e81b7c45d87a34f111472bd66320146de35847
PanSedtabodee <88127782+PanSedtabodee@users.noreply.github.com>
2023-02-24 19:31:47 +0700

    Calculator version 3 and add Percent

--- README.md
+++ README.md
@@ -1,11 +1,85 @@
-Member
 
-64130500081 นาย เศรษฐบฎี บุญพิทักษ์
+# Calculator 
 
-64130500083 นายสหรัฐ คนึงเหตุ
+**This is a web about calculating numbers**
 
-64130500098 นายณัฐชนน บุญชู
+## Features :pushpin:
 
-64130500118 นางสาวชนากานต์ นิลวัฒน์
+- **C** : clear all and start a new operation
+- **+/-** : change the sign of the displayed value from positive to negative
+- **←** : subtract last digit numbers (backspace)
+- **÷** : divide value 
+- **x** : multiply value
+- **-** : minus value
+- **+** : plus value
+- **=** : execute calculation
+- **History** : Keep and show history that we have calculated.
 
-64130500121 นายรามิล ปิยานุลักษณ์
+## How to use :pushpin:
+**Basic Operations:**
+Our calculator has basic functions of a calculator are addition, subtraction, multiplication, and division. For example, if you want to add 1 and 2, press the following keys: 1 + 2 = (results displayed). Similarly, for subtraction, multiplication, and division.
+<br><br>
+**Order of Operations:**
+A calculator follows the order of operations, which means that it performs calculations in a specific order. The order of operations is as follows: parentheses first, then exponents, then multiplication and division (from left to right), and finally addition and subtraction (from left to right). For example, to solve the expression 3 + 5 x 2, you would first multiply 5 x 2 and then add the result to 3.
+
+**Decimal and Negative Numbers:**
+To input a decimal number, press the "." key. For example, to input the number 3.14, press 3 . 1 4 = (results displayed). To input a negative number, press the "-" key before the number. For example, to input the number -5, press -5 = (results displayed).
+<br><br>
+**History Functions:**
+Our calculator has memory functions that allow you to store and recall values and also has clear function.
+
+- Operator : ใช้งานสัญลักษณ์ทางคณิตศาสตร์<br>
+![operatorgif](https://user-images.githubusercontent.com/88131673/220914456-7e3d3d6c-aac4-4f7e-8d00-7fe8dedafafc.gif)
+
+- Backspace : ใช้งานเพื่อลบตัวหลังสุด<br>
+![backspacegif](https://user-images.githubusercontent.com/88131673/220914485-a7a8839b-2c90-4751-b9cf-4d5590236ea6.gif)
+
+- Sign : ใช้ในการเปลี่ยนค่าของข้อมูลจาก positive เป็น negative หรือกลับกัน<br>
+![signgif](https://user-images.githubusercontent.com/88131673/220914522-160d8547-9478-40bf-9f19-4ee7a3ae0f51.gif)
+
+- History :กดปุ่มHistory เพื่อแสดงหน้าต่างที่เก็บประวัติการใช้งาน<br>
+![history2gif](https://user-images.githubusercontent.com/88131673/220918223-e934a040-781f-4b5b-a368-a0c145d0aee0.gif)
+
+
+## References :pushpin:
+- [Calculator structure](https://www.youtube.com/watch?v=m1_ih43p24s )
+- [History](https://www.youtube.com/watch?v=IM1RzqlX2JA)
+ - [**tailwind CSS**](https://tailwindcss.com/)
+> - [Position](https://tailwindcss.com/docs/position#absolutely-positioning-elements) <br>
+> - [Ring-width](https://tailwindcss.com/docs/ring-width)<br>
+> - [Ring-color](https://tailwindcss.com/docs/ring-color)<br>
+> - [Ring-offset-color](https://tailwindcss.com/docs/ring-offset-color)<br>
+> - [text-overflow](https://tailwindcss.com/docs/text-overflow)<br>
+
+- **Theme**
+> - [Color]([https://monkeytype.com/](https://monkeytype.com/settings#group_theme))= JOKER<br>
+![Screenshot 2023-02-22 221812](https://user-images.githubusercontent.com/88131673/220668253-0ae62dcf-09c1-43d3-9a31-a5e047ce9c00.png)<br>
+
+>- **Code Color**<br>
+![Screenshot 2023-02-22 222344](https://user-images.githubusercontent.com/88131673/220668523-5b414018-a0fd-48ad-9ecf-d008c7ffdea4.png)
+
+## Member :pushpin:
+
+สมาชิกทุกคนแบ่งกันไป coding calculator ในเวอร์ชั่นของตนเองในตอนแรกแล้วนำมาเลือกส่วนที่ดีของแต่ละคนออกมาใช้
+- :construction_worker: 64130500081 [Sedtabodee Bonphitak](https://github.com/PanSedtabodee) 20%<br>
+หน้าที่: ขึ้นโครงเครื่องคิดเลขเบื้องต้นให้เพื่อน , ทำ Responsive , debug
+>  แก้ปัญหา text overflow โดยการตัดทศนิยม
+
+- :construction_worker: 64130500083 [Saharat Kanuenghade](https://github.com/KenMuey) 20%<br>
+หน้าที่: create feature and debug จากโค้ดหลักของเพื่อน เช่น<br>
+> ไม่ให้กด 0 ซ้ำได้ในตอนเริ่มโปรแกรม<br>
+> ทำให้ 0 ไม่ติดลบ<br>
+> แก้ไข้การใช้งาน dot<br>
+> ไม่ให้สามารถกด = ได้ตั้งแต่เริ่มโปรแกรม
+> ทำให้ผลลัพธ์เป็น 0 เเล้วไม่สามารถกด 0 ซ้ำได้
+
+- :construction_worker: 64130500098 [Natchanon Boonchu](https://github.com/NatchanonBoonchu) 20%<br>
+หน้าที่: เป็นที่ปรึกษาของเพื่อนๆในกลุ่ม เป็น main code ทำ functionแสดงสมการ, function เก็บHistory, ตกแต่ง tailwind CSS ,comment code อธิบาย และ debugบางส่วน เช่น 
+> ไม่ให้กด "." เป็นตัวแรก และไม่ให้กด "." ซ้ำ<br>
+> ดักไม่ให้กดเครื่องหมายเป็นตัวแรก
+
+- :construction_worker: 64130500118 [Chanakarn Ninlawat](https://github.com/Saiparnn) 20%<br>
+หน้าที่: ลองเขียนฟังก์ชั่นในรูปแบบอื่นๆเพื่อให้สมาชิกลองนำมาใช้
+
+- :construction_worker: 64130500121 [Ramin Piyanuluk](https://github.com/Tiger4846) 20%<br>
+หน้าที่: ออกเเบบเเละหา function ,coding ของตัวเองขึ้น ,ตกแต่ง tailwind CSS ,ทำ README
--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,297 @@
-<script>
-export default {
-  name: 'Calculator',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      calculatorValue: '',
-      calculatorElements: ['C', 'X', '÷', '-', 7, 8, 9, '+', 4, 5, 6, '%', 1, 2, 3, '=', 0, '.'],
-      operator: null,
-      previousCalculatorValue: '',
-    }
-  },
+<script setup>
+import { ref } from 'vue';
+let current = ref('');
+let previousNumber = ref('');
+let operator = ref(null);
+let currentOperator = ref('');
+let operatorClicked = ref(false); 
+let equation = ref(''); // เอาไว้เก็บสมการ(ที่ขึ้นตัวๆเล็กด้านบน)
+let clickedEqual = ref(false) // ถ้ากด = แล้วมันจะเปลี่ยนเป็น true | พอเป็น true แล้วจะไปดักไม่ให้กด = ซ้ำ | แล้วไปเซ็ตค่ากลับเป็น false เวลากดปุ่มตัวเลขที่function append
 
-  methods: {
-    action(n) {
+// History
+let histories = ref([])
+let result = ref('')
+// ปุ่มtoggle history
+let toggleHistory = ref(false)
 
-      // Append Value
-      if (!isNaN(n) || n === '.') {
-        this.calculatorValue += n + '';
-      }
+function clear() {
+  current.value = '';
+  equation.value = '';
+}
 
-      // Clear value
-      if (n === 'C') {
-        this.calculatorValue = '';
-      }
-
-      // Percentage
-      if (n === '%') {
-        this.calculatorValue = this.calculatorValue / 100
-      }
-
-
-      if (['X', '÷', '-', '+'].includes(n)) {
-        if (n === '÷') {
-          this.operator = '/'
-        }
-        else {
-          this.operator = n
-        }
-        this.previousCalculatorValue = this.calculatorValue
-        this.calculatorValue = '';
-      }
-
-      if (n === '=') {
-        this.calculatorValue = eval(
-          this.previousCalculatorValue + this.operator + this.calculatorValue
-        );
-
-        this.previousCalculatorValue = ''
-        this.operator = null;
-      }
-    }
+function sign() {
+  if(current.value > 0 || current.value < 0) { // ดักไม่ให้ 0 ติดลบ
+    current.value = current.value.charAt(0) === "-" ? current.value.slice(1) : `-${current.value}`;
   }
 }
+
+function del() {
+  current.value = `${current.value.slice(0, -1)}`;
+}
+
+function append(number) {
+  clickedEqual = false;
+  if (operatorClicked || current.value === '0') {
+    current.value = "";
+    operatorClicked = false;
+  }
+  if(current.value.length > 10){ // ไม่ให้กดตัวเลขเกิน 11 ตัว
+    return current.value
+  }
+  if(current.value === 0) {
+    return current.value = number
+  }
+  current.value = `${current.value}${number}`;
+}
+
+function setPreviousNumber() {
+  previousNumber.value = current.value; // เปลี่ยนตัวเลขcurrent --> previous (ตัวเลขตัวแรก)  -->A<-- + B = X
+  operatorClicked = true; // เมื่อกดเครื่องหมายจะเปลี่ยนเป็น true แล้วไปเข้าเงื่อนไขใน function append
+}
+
+function dot() {
+  if (current.value.includes(".") || current.value === "") { // ดักไม่ให้กด'.'ซ้ำและกดตัวแรก
+    return;
+  }else if(current.value === '0'){
+    append("0.");
+  }else {
+    append(".");
+  }
+}
+
+function setResultAfterClickOperan() {
+  result.value = `${current.value} ${currentOperator}`; // [History] ตั้งค่าสมการที่จะเก็บไว้ใน history เก็บไว้ในตัวแปล result Ex.'3 +'
+}
+
+function setResultAfterClickPercent() {
+  result.value = `${previousNumber.value} ${currentOperator}`; 
+  result.value = result.value + ' ' + '=' + ' ' + current.value
+  histories.value.push(result.value);
+}
+
+function divide() {
+  if (current.value === "") {  // ดักไม่ให้กดเครื่องหมายเป็นตัวแรก
+    return;
+  } else {
+    operator = (a, b) => a / b;
+    currentOperator = "÷";
+    setEquation();
+    setResultAfterClickOperan();
+    setPreviousNumber();
+  }
+}
+
+function multiply() {
+  if (current.value === "") {
+    return;
+  } else {
+    operator = (a, b) => a * b;
+    currentOperator = "x";
+    setEquation();
+    setResultAfterClickOperan();
+    setPreviousNumber();
+  }
+}
+
+function minus() {
+  if (current.value === "") {
+    return;
+  } else {
+    operator = (a, b) => a - b;
+    currentOperator = "-";
+    setEquation();
+    setResultAfterClickOperan();
+    setPreviousNumber();
+  }
+}
+
+function plus() {
+  if (current.value === "") {
+    return;
+  } else {
+    operator = (a, b) => a + b;
+    currentOperator = "+";
+    setEquation();
+    setResultAfterClickOperan();
+    setPreviousNumber();
+  }
+}
+
+function percent() {
+  if (current.value === "") {
+    return;
+  } else if (current.value == 0){ //ดักไม่ให้กดเมื่อมีค่าเป็น 0 และกดเปอร์เซ็นต์  //ที่ใช้ = สองตัวเพราะว่าต้องการให้เช็คว่ามีค่าเท่ากับ 0 ไหม โดยไม่เช็ค Type
+    current.value = 0;
+  } else {
+    setPreviousNumber();
+    current.value = parseFloat(current.value/100).toFixed(5)
+    currentOperator = "%";
+    setEquationPercent();
+    setResultAfterClickPercent();
+  }
+}
+
+function setEquation() {
+  equation.value = `${current.value} ${currentOperator}`; // [Equation] แสดงสมการหลังจากที่กดเครื่องหมาย Ex. '3 +'
+}
+
+function setEquationPercent(){
+  equation.value = `${previousNumber.value} ${currentOperator} = `
+}
+
+function equal() {
+  if (clickedEqual) { // ดักไม่ให้กด = ซ้ำ
+    return;
+  }else if(previousNumber.value === '' || previousNumber.value === null){ // ดักไม่ให้กด = ก่อนเครื่องหมาย
+    return current.value
+  }else {
+    equation.value = `${previousNumber.value} ${currentOperator} ${current.value} = `; // [Equation] แสดงสมการหลังจากที่กด '=' Ex. '3 + 5 ='
+    result.value = result.value + ' ' +current.value // [History] ก่อนจะคำนวณ เอาresultจากsetResultAfterClickOperan()มาใช้ต่อ จะได้ '3 + 5'
+    current.value = operator(parseFloat(previousNumber.value), parseFloat(current.value));
+    if(!Number.isInteger(current.value)){ // ถ้าcurrent ไม่ใช่จำนวนเต็ม ให้แปลงเป็นFloat ก่อน แล้วค่อยtoFixed() ตำแหน่งทศนิยม
+      current.value = parseFloat(current.value).toFixed(5)
+    }
+    result.value = result.value + ' = ' +current.value // [History] หลังจากคำนวณ เอาresultอันก่อนหน้านี้มาใช้ต่อ จะได้ '3 + 5 = 8'
+    histories.value.push(result.value) // [History] push result เข้าไปเก็บใน history
+    previousNumber.value = null;
+    clickedEqual = true; 
+  }
+}
+
+function clearHistory() {
+  histories.value = []
+}
+
 </script>
 
 <template>
-  <div class="h-screen bg-vue-dark py-[50px]">
-
-    <div class="w-full p-3 max-w-[400px] mx-auto bg-[#234] rounded">
-
-      <!-- Calculator Result -->
-      <div class="w-full rounded my-1 p-3 text-right font-weight-bold text-white bg-vue-dark">
-        {{ calculatorValue || 0}}
-      </div>
-
-      <!-- Calculator buttons -->
-      <div class="grid grid-cols-4 gap-[0.0005rem]">
-        <div v-for="n in calculatorElements" :key="n">
-          <div class="rounded text-white text-center m-1 py-4 bg-vue-dark hover-class"
-            :class="{ 'bg-vue-green': ['C', 'X', '÷', '-', '+', '%', '='].includes(n) }" @click="action(n)">
-            {{ n }}
+  <div class="flex min-h-screen bg-[#1a0e25] items-center justify-center max-[1000px]:flex-col  select-none">
+    <div class="bg-purple-900 p-10 rounded-lg m-10">
+      <h1 class="text-5xl text-center pb-7 font-semibold text-white">Calculator</h1>
+      <div class="calculator bg-[#14081f] text-3xl w-96 p-8">
+        <div class="display bg-[#301652] col-span-4 pl-2 rounded-lg mb-1 text-right pr-2 relative overflow-hidden ">
+          <div class="text-gray-200 text-2xl">
+            {{ equation }}
+          </div>
+          <div class="currentDisplay text-white text-5xl font-bold absolute bottom-1 right-2 ">
+            {{ current || 0 }}
           </div>
         </div>
+        <div @click="clear()" class="btn">C</div>
+        <div @click="sign()" class="btn">+/-</div>
+        <div @click="del()" class="btn">←</div>
+        <div @click="divide()" class="btn operator">÷</div>
+        <div @click="append('7')" class="btn">7</div>
+        <div @click="append('8')" class="btn">8</div>
+        <div @click="append('9')" class="btn">9</div>
+        <div @click="multiply()" class="btn operator">x</div>
+        <div @click="append('4')" class="btn">4</div>
+        <div @click="append('5')" class="btn">5</div>
+        <div @click="append('6')" class="btn">6</div>
+        <div @click="minus()" class="btn operator">-</div>
+        <div @click="append('1')" class="btn">1</div>
+        <div @click="append('2')" class="btn">2</div>
+        <div @click="append('3')" class="btn">3</div>
+        <div @click="plus()" class="btn operator">+</div>
+        <div @click="append('0')" class="btn">0</div>
+        <div @click="dot()" class="btn">.</div>
+        <div @click="percent()" class="btn operator">%</div>
+        <div @click="equal()" class="btn operator">=</div>
+      </div>
+      <div @click="toggleHistory = !toggleHistory" class="historyBtn hover:ring-4 hover:ring-offset-2 ring-[#14081f]">
+        <h1>HISTORY</h1>
 
       </div>
     </div>
+
+    <div v-show="toggleHistory" class="bg-purple-900 pt-8 px-10 ml-7 rounded-lg max-[1000px]:mb-10 max-[1000px]:ml-0">
+      <h1 class="text-5xl text-center pb-7 font-semibold text-white">History</h1>
+      <div class="bg-gray-200 rounded-lg overflow-y-auto w-96 h-96">
+        <ul class="text-lg pl-3 pt-2">
+            <li v-for="history in histories">{{ history }}</li>
+        </ul>
+      </div>
+      <div class="clearHistoryBtn hover:ring-4 hover:ring-offset-2 ring-[#99de1e]">
+        <h1 @click="clearHistory() ">Clear history</h1>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style scoped>
-.bg-vue-dark {
-  background: #31475e;
-}
 
-.hover-class:hover {
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(4, 80px);
+  grid-template-rows: repeat(6, 80px);
+  border-radius: 5%;
+  user-select: none;
+}
+.btn {
+  color: white;
+  background-color: #462870;
+  border: 1px solid black;
+  border-radius: 10px;
+  text-align: center;
+  margin: 3px;
+  padding: 14px;
+}
+.btn:hover {
+  color: #341f50;
+  transition-duration: 0.4s;
+  background-color: #e2e8f0;
   cursor: pointer;
-  background: #3D5875;
 }
-
-.bg-vue-green {
-  background: #3fb984;
+.btn:active {
+  transform: scale(1.2, 1.2);
+}
+.operator {
+  background-color: #99de1e;
+  color: #341f50;
+}
+.operator:hover {
+  background-color: #e2e8f0;
+  color: #341f50;
+}
+.historyBtn {
+  background-color: #14081f;
+  border: 1px solid black;
+  color: white;
+  font-size: x-large;
+  font-weight: bold;
+  border-radius: 10px;
+  text-align: center;
+  margin-top: 25px;
+  padding: 14px;
+  user-select: none;
+}
+.historyBtn:hover {
+  transition-duration: 0.4s;
+  background-color: #e2e8f0;
+  color: #14081f;
+}
+.historyBtn:active{
+  transform: scale(1.1, 1.1);
+}
+.clearHistoryBtn{
+  background-color: #99de1e;
+  border: 1px solid black;
+  color: #341f50;
+  font-weight: bold;
+  border-radius: 10px;
+  text-align: center;
+  margin: 25px 30px;
+  padding: 5px;
+  user-select: none;
+}
+.clearHistoryBtn:hover{
+  transition-duration: 0.4s;
+  background-color: #e2e8f0;
+  color: #341f50;
+  border: 1px solid #99de1e;
+}
+.clearHistoryBtn:active{
+  transform: scale(1.1, 1.1);
 }
 </style>
